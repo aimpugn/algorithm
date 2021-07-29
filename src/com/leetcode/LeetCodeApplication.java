cf7bd30478eb809a07c4f1692158a37b5f0384b8
aimpugn <aimpugn@gmail.com>
2021-07-30 00:36:19 +0900

    125, 344 문제 풀이

--- src/com/leetcode/LeetCodeApplication.java
+++ src/com/leetcode/LeetCodeApplication.java
@@ -2,12 +2,13 @@ package com.leetcode;
 
 
 import com.leetcode.common.entity.SolutionInterface;
+import com.leetcode.solutions.ReverseString;
 import com.leetcode.solutions.ValidPalindrome;
 
 public class LeetCodeApplication {
 
     public static void main(String[] args) {
-        SolutionInterface solutionInterface = new ValidPalindrome();
+        SolutionInterface solutionInterface = new ReverseString();
         solutionInterface.doTest();
     }
 }
