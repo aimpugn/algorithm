e48d83c398272ef0b762004b78dd2b3522b7f661
aimpugn <aimpugn@gmail.com>
2021-11-30 01:05:05 +0900

    프로젝트명, 디렉토리명 등 변경

--- app/src/main/java/com/algo/AlgorithmApplication.java
+++ app/src/main/java/com/algo/AlgorithmApplication.java
@@ -4,7 +4,7 @@ package com.algo;
 import com.algo.common.entity.SolutionInterface;
 import com.algo.programmers.p42577.Solution;
 
-public class LeetCodeApplication {
+public class AlgorithmApplication {
 
     public static void main(String[] args) {
         SolutionInterface solutionInterface = new Solution();
